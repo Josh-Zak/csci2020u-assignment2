6eec93076dd638cbb0c3955da97c74ed493decca
Chau Nguyen <chau.nguyen@ontariotechu.net>
2021-03-30 20:36:16 +0700

    create the user interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,26 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+
 
 public class Controller {
 
 //find out how to connect this to the client for the UI
+    @FXML
+    ListView local;
+    @FXML
+    ListView server;
 
     @FXML
     public void initialize() {
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Single", "Double", "Suite", "Family App");
+        local.setItems(items);
 
     }
 }
